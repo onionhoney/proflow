fa05f8832487e39744d660c73097685da4441681
onionhoney <zhouhengsun@gmail.com>
2016-02-13 15:45:35 -0800

    Setup HTML layout and created files.

---
+++ view.js
@@ -0,0 +1,26 @@
+
+// ===============================
+//           UI Updates
+// ===============================
+
+function renderStatus(statusText) {
+  document.getElementById('status').textContent = statusText;
+}
+
+
+
+// ===============================
+//         Main Eventloop
+// ===============================
+
+document.addEventListener('DOMContentLoaded', function() {
+     
+});
+
+
+// Logic:
+// List view, expansion, 
+// TODO:
+// Add list to html
+// You can put things in <li> the same way you would a <div> tag.
+// Set its style...
