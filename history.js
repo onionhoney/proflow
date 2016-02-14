a65858d3c38a3fefa9ca2ec6599a649cf525ca88
Xinyu Yang <colin.yangxinyu@outlook.com>
2016-02-13 17:12:02 -0800

    make history list sorted

--- history.js
+++ history.js
@@ -3,25 +3,28 @@ var hist = {};
 hist.cache = {};
 
 hist.add = function(entry, result) {
-    entry = entry.trim().split(/\s+/).join(' ')
+    entry = entry.trim().split(/\s+/).join(' ');
     this.cache[entry] = result.trim();
 };
 
 hist.del = function(entry) {
-    delete this.cache[entry.trim()];
+    entry = entry.trim().split(/\s+/).join(' ');
+    delete this.cache[entry];
 };
 
 function render($list) {
     $list.innerHTML = '';
 
-    for (var entry in hist.cache) {
+    entries = Object.keys(hist.cache);
+    entries.sort();
+    entries.forEach(function (entry) {
 
         var ul = document.createElement('ul');
 
         var entry_node = document.createElement("span");
         entry_node.innerHTML = entry;
         var entry_attr = document.createAttribute("class");
-        entry_attr.value = "entry"
+        entry_attr.value = "entry";
         entry_node.setAttributeNode(entry_attr);
 
         var result_node = document.createElement("span");
@@ -34,5 +37,5 @@ function render($list) {
         ul.appendChild(result_node);
 
         $list.appendChild(ul);
-    }
+    });
 }
