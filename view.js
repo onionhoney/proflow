2a0dbb71246ecb891e1ffe8c7faf9089e204c794
onionhoney <zhouhengsun@gmail.com>
2016-02-14 01:54:48 -0800

    Corrected async and removed garbage.

--- view.js
+++ view.js
@@ -1,36 +1,10 @@
 $(function(){
-
     // Constants
     // TODO: set colors based on hash
     var DEBUG = true;
     // Initialize variables
     var $window = $(window);
     var $bar = $(".bar");
-
-    // doesn't work, acquire dom dynamically
-    // var $list = $("#list-container");
-    // hist.init($list);
-
-    // function doSearch(){
-    //     var query = $bar.val();
-    //     var url = "https://www.googleapis.com/customsearch/v1?q=" + encodeURI(query);
-    //     url += "&cx=" + CX + "&key=" + API_KEY;
-    //     if (DEBUG){
-    //         var data = googleAPISampleJSON;
-    //         console.log(data);
-    //         var firstEntry = data.items[0];
-    //         var message = firstEntry.snippet;
-    //         hist.add(query, message);
-               // change list-container to hist-list-container
-    //         var $list = $("#list-container");
-    //         hist.render($list);
-    //     }
-    //     else{
-    //         $.get(url, function(data){
-    //             console.log(data);
-    //         });
-    //     }
-    // }
 
     // Keyboard events
     $window.keydown(function (event) {
@@ -46,14 +20,13 @@ $(function(){
 
     // Click events
 
-    // make search when search button is clicked
+    // Make search when search button is clicked
     $(".search-icon").on("click", function() {
         hist.doSearch($bar.val());
     });
 
     // Toggle result when user click on an entry
     $("#list-container").on("click", ".entry", function() {
-
         $(this).next().toggleClass("result-active");
     });
 
