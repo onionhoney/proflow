faa031cda0305467508126a816d7652631c78a74
onionhoney <zhouhengsun@gmail.com>
2016-02-13 19:42:16 -0800

    Integrated google API

--- history.js
+++ history.js
@@ -1,3 +1,31 @@
+var API_KEY = "AIzaSyAe0ReD5igVVJFmDMJKHCAOU3nRHT4E2As";
+var CX = "011043985394505284497%3A04toexks_gs"; 
+
+
+function doSearch(){
+        var query = $bar.val();
+        var url = "https://www.googleapis.com/customsearch/v1?q=" + encodeURI(query);
+        url += "&cx=" + CX + "&key=" + API_KEY;
+        
+        
+        if (DEBUG){
+            var data = googleAPISampleJSON;
+            console.log(data);
+            var firstEntry = data.items[0];
+            var message = firstEntry.snippet;
+            hist.add(query, message); 
+            hist.render($list);
+        }
+        else{
+            $.get(url, function(data){
+                console.log(data);
+            });
+        }
+    }
+
+
+
+
 
 var hist = {};
 hist.cache = {};
@@ -12,22 +40,22 @@ hist.del = function(entry) {
     delete this.cache[entry];
 };
 
-function render($list) {
+hist.render = function($list) {
     $list.innerHTML = '';
 
-    entries = Object.keys(hist.cache);
+    entries = Object.keys(this.cache);
     entries.sort();
     entries.forEach(function (entry) {
 
-        var ul = document.createElement('ul');
+        var ul = document.createElement('li');
 
-        var entry_node = document.createElement("span");
+        var entry_node = document.createElement("div");
         entry_node.innerHTML = entry;
         var entry_attr = document.createAttribute("class");
         entry_attr.value = "entry";
         entry_node.setAttributeNode(entry_attr);
 
-        var result_node = document.createElement("span");
+        var result_node = document.createElement("div");
         result_node.innerHTML = hist.cache[entry];
         var result_attr = document.createAttribute("class");
         result_attr.value = "result";
@@ -38,4 +66,4 @@ function render($list) {
 
         $list.appendChild(ul);
     });
-}
+};
--- view.js
+++ view.js
@@ -1,26 +1,53 @@
+$(function(){
 
-// ===============================
-//           UI Updates
-// ===============================
+    // Constants
+    // TODO: set colors based on hash
+    var API_KEY = "AIzaSyAe0ReD5igVVJFmDMJKHCAOU3nRHT4E2As";
+    var CX = "011043985394505284497%3A04toexks_gs"; 
+    var DEBUG = true;
+    // Initialize variables
+    var $window = $(window);
+    var $bar = $(".bar");
+    var $list = $("#list-container");
 
-function renderStatus(statusText) {
-  document.getElementById('status').textContent = statusText;
-}
+    function doSearch(){
+        var query = $bar.val();
+        var url = "https://www.googleapis.com/customsearch/v1?q=" + encodeURI(query);
+        url += "&cx=" + CX + "&key=" + API_KEY;
+        
+        
+        if (DEBUG){
+            var data = googleAPISampleJSON;
+            console.log(data);
+            var firstEntry = data.items[0];
+            var message = firstEntry.snippet;
+            hist.add(query, message); 
+            hist.render($list);
+        }
+        else{
+            $.get(url, function(data){
+                console.log(data);
+            });
+        }
+    }
+    // Keyboard events
+   
+    $window.keydown(function (event){
+        // Auto-focus the bar
+        if (!(event.ctrlKey || event.metaKey || event.altKey)) {
+            $bar.focus();
+        }
+        // When the user hits ENTER, launch search
+        if (event.which === 13){
+            doSearch();
+        }
+    });
 
+    // Click events
 
+    // Toggle result when user click on an entry
+    $(".entry").on("click", function(){
+        $(".result").toggleClass("result-active");
+    });
 
-// ===============================
-//         Main Eventloop
-// ===============================
-
-document.addEventListener('DOMContentLoaded', function() {
-     
 });
-
-
-// Logic:
-// List view, expansion, 
-// TODO:
-// Add list to html
-// You can put things in <li> the same way you would a <div> tag.
-// Set its style...
